dcacfeda665622c24ff6e629843701fcc3b4c6f6
admin <admin@jvu-2.local>
2024-04-12 13:06:00 +0700

    home

--- app/page.js
+++ app/page.js
@@ -1,4 +1,20 @@
 // `app/page.js` is the UI for the `/` URL
+// Home page.
+
 export default function Page() {
-  return <h1>Hello, Home page!</h1>
+  return (
+    <div className="home">
+
+      <video muted loop autoPlay>
+        <source src="/home/Frank_demo.mp4" type="video/mp4" />
+      </video>
+
+      {/*<div className="home-content">*/}
+      {/*  <h1></h1>*/}
+      {/*  <p></p>*/}
+
+      {/*  <button></button>*/}
+      {/*</div>*/}
+    </div>
+  )
 }
