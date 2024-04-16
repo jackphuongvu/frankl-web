ee0bf75347b1b9f2fd37d20cdcb08132a3cf0fe4
MACBOOK <macbook@jack.local>
2024-04-16 14:27:51 +0700

    import a dist file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
 // @ts-check
+const CopyPlugin = require("copy-webpack-plugin");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
   output: 'export',
+  webpack: (config) => {
+    // append the CopyPlugin to copy the file to your public dir
+    config.plugins.push(
+        new CopyPlugin({
+          patterns: [
+            { from: "node_modules/journal/dist", to: "public/" },
+          ],
+        }),
+    )
+
+    // Important: return the modified config
+    return config
+  }
 }
 
 module.exports = nextConfig
--- pages/home/HomePage.js
+++ pages/home/HomePage.js
@@ -1,22 +1,43 @@
 import './home.css'
 
-const HomePage = () => (
-    <div className="home">
+import {Button} from "@nextui-org/react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-        <video
-        preload="auto"
-        muted 
-        loop
-        autoPlay>
-            <source src="/home/Frank_demo.mp4" type="video/mp4" />
-        </video>
+const HomePage = () => {
+    const openFrankl = () => {
 
-        {/*<div className="home-content">*/}
-        {/*  <h1></h1>*/}
-        {/*  <p></p>*/}
+    }
 
-        {/*  <button></button>*/}
-        {/*</div>*/}
-    </div>
-)
+    return (
+        <div className="home">
+
+            <video
+                preload="auto"
+                muted
+                // loop
+                // autoPlay
+            >
+                <source src="/home/Frank_demo.mp4" type="video/mp4" />
+            </video>
+
+            <div className="home-content">
+                {/*<h1></h1>*/}
+                {/*<p></p>*/}
+
+
+                <Button
+                    isIconOnly
+                    className="w-auto h-auto data-[hover]:bg-foreground/10"
+                    radius="full"
+                    variant="light"
+                >
+                    {/*<i className="fa-solid fa-play"></i>*/}
+                    <FontAwesomeIcon icon="fa-solid fa-play" />
+                    {/*<PauseCircleBoldIcon size={54} />*/}
+                </Button>
+
+            </div>
+        </div>
+    )
+}
 export default HomePage;
